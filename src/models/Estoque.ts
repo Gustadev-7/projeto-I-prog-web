c74ee5fde103b72f7261e364f0357bd5702e7419
Gustadev-7 <gustavo.souzadomingos7@gmail.com>
2026-05-23 10:19:19 -0300

    feat: adicionar classe Estoque

---
+++ src/models/Estoque.ts
@@ -0,0 +1,19 @@
+export class Estoque {
+    id_estoque: number
+    id_carro: number
+    quantidade: number
+    localizacao_patio: string
+    data_entrada: Date
+
+    constructor(id_carro: number, quantidade:number, localização_patio: string, data_entrada: Date) {
+        this.id_estoque = this.geraId();
+        this.id_carro = id_carro;
+        this.quantidade = quantidade;
+        this.localizacao_patio = localização_patio;
+        this.data_entrada = data_entrada;
+    }
+
+    private geraId():number {
+        return Date.now();
+    }
+}
