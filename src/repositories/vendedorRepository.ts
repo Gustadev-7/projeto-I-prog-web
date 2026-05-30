a7ae8a506243b10a0bdf7003a17d9625cddaab1a
gabsduo <bieldemisilva@gmail.com>
2026-05-29 23:00:12 -0300

    finalização repositório vendedor

--- src/repositories/vendedorRepository.ts
+++ src/repositories/vendedorRepository.ts
@@ -6,6 +6,7 @@ export class vendedorRepository{
 
     private constructor () {}
 
+    //singleton para garantir que haja apenas uma instancia do repositório
     public static getInstance(): vendedorRepository{
         if(!this.instance){
             this.instance = new vendedorRepository();
@@ -13,7 +14,39 @@ export class vendedorRepository{
         return this.instance;
     }
 
+    //cadastrar vendedor
     insereVendedor(vendedor: Vendedor){
         this.vendedorList.push(vendedor)
+    }
+
+    //listar vendedores
+    listarVendedores(): Vendedor[]{
+        return this.vendedorList;
+    }
+
+    //filtrar vendedor por id
+    filtraVendedorPorId(id_vendedor: Number): Vendedor | undefined{
+        return this.vendedorList.find(vendedor => vendedor.id_vendedor === id_vendedor);
+    }
+    
+    //atualizar vendedor por id
+    atualizarVendedor(id_vendedor: Number, dados: Partial<Vendedor>): Vendedor | undefined {
+        const vendedor = this.vendedorList.find(vendedor => vendedor.id_vendedor === id_vendedor);
+
+        if (!vendedor) return undefined;
+
+        Object.assign(vendedor, dados);
+        return vendedor;
+    }
+
+    //deletar vendedor por id
+    deletarVendedor(id_vendedor: Number): Vendedor | undefined {
+        const vendedor = this.vendedorList.findIndex(vendedor => vendedor.id_vendedor === id_vendedor);
+        if(vendedor === -1) return undefined;
+
+        const vendedorDeletado = this.vendedorList[vendedor];
+
+        this.vendedorList.splice(vendedor, 1);
+        return vendedorDeletado;
     }
 }
