54b4f07dad1106591ab270e8ffea70eb8529294a
gabsduo <bieldemisilva@gmail.com>
2026-05-29 23:14:05 -0300

    adicionando repositório para estoque

---
+++ src/repositories/estoqueRepository.ts
@@ -0,0 +1,57 @@
+//Repositório Estoque
+import { Estoque } from "../models/Estoque"
+
+export class estoqueRepository{
+    private static instance: estoqueRepository
+    private estoqueList: Estoque[] = [];
+
+    private constructor () {}
+
+    //singleton para garantir que haja apenas uma instancia do repositório
+    public static getInstance(): estoqueRepository{
+        if(!this.instance){
+            this.instance = new estoqueRepository();
+        }
+        return this.instance;
+    }
+
+    //cadastrar estoque
+    insereEstoque(estoque: Estoque){
+        this.estoqueList.push(estoque)
+    }
+
+    //listar estoque
+    listarEstoque(): Estoque[]{
+        return this.estoqueList;
+    }
+
+    //filtrar estoque por id
+    filtraEstoquePorId(id_estoque: Number): Estoque | undefined{
+        return this.estoqueList.find(estoque => estoque.id_estoque === id_estoque);
+    }
+
+    //filtrar estoque por id do carro, retorna o estoque relacionado a um carro específico
+    listarEstoquePorCarro(id_carro: Number): Estoque[] {
+        return this.estoqueList.filter(estoque => estoque.id_carro === id_carro);
+    }
+
+    //atualizar estoque por id
+    atualizarEstoque(id_estoque: Number, dados: Partial<Estoque>): Estoque | undefined {
+        const estoque = this.estoqueList.find(estoque => estoque.id_estoque === id_estoque);
+
+        if (!estoque) return undefined;
+
+        Object.assign(estoque, dados);
+        return estoque;
+    }
+
+    //deletar estoque por id
+    deletarEstoque(id_estoque: Number): Estoque | undefined {
+        const estoque = this.estoqueList.findIndex(estoque => estoque.id_estoque === id_estoque);
+        if(estoque === -1) return undefined;
+        const estoqueDeletado = this.estoqueList[estoque];
+
+        this.estoqueList.splice(estoque, 1);
+        return estoqueDeletado;
+    }
+}
--- src/repositories/vendedorRepository.ts
+++ src/repositories/vendedorRepository.ts
@@ -31,21 +31,28 @@ export class vendedorRepository{
     
     //atualizar vendedor por id
     atualizarVendedor(id_vendedor: Number, dados: Partial<Vendedor>): Vendedor | undefined {
+
+        //encontra o vendedor a ser atualizado usando o id fornecido
         const vendedor = this.vendedorList.find(vendedor => vendedor.id_vendedor === id_vendedor);
 
         if (!vendedor) return undefined;
 
+        //atualiza os dados do vendedor encontrado com os novos dados fornecidos
         Object.assign(vendedor, dados);
         return vendedor;
     }
 
     //deletar vendedor por id
     deletarVendedor(id_vendedor: Number): Vendedor | undefined {
+
+        //encontra o índice do vendedor a ser deletado
         const vendedor = this.vendedorList.findIndex(vendedor => vendedor.id_vendedor === id_vendedor);
         if(vendedor === -1) return undefined;
 
+        //armazena o vendedor a ser deletado para retornar depois da remoção
         const vendedorDeletado = this.vendedorList[vendedor];
 
+        //remove o vendedor da lista usando o índice encontrado
         this.vendedorList.splice(vendedor, 1);
         return vendedorDeletado;
     }
